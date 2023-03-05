f7716a0827ed6a1d405f28a0012bbc27298f4462
Mayank Choudhary <mayankchoudhary@engineer.com>
2023-03-05 22:28:30 +0530

    Final tune-ups.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Mayank &nbsp;
+            Mayank Choudhary &nbsp;
             <span className='sm:block hidden'> | Full Stack Developer</span>
           </p>
         </Link>
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -80,11 +80,7 @@ const Works = () => {
           variants={fadeIn("", "", 0.1, 1)}
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
-          Following projects showcases my skills and experience through
-          real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
-          ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
+          Following are the projects I have worked on so far. I have provided a brief description of each project. Please feel free to reach out in case you are interested in learning more about me or my projects.
         </motion.p>
       </div>
 
