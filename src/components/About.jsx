921d3dd49e3143a021108ed1059943394594f56e
balkar1998 <balkar48812@gmail.com>
2023-12-05 20:15:17 +0530

    first commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Tilt from "react-tilt";
+// import Tilt from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -8,7 +8,7 @@ import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => (
-  <Tilt className='xs:w-[250px] w-full'>
+  // <Tilt className='xs:w-[250px] w-full'>
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
       className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
@@ -32,7 +32,7 @@ const ServiceCard = ({ index, title, icon }) => (
         </h3>
       </div>
     </motion.div>
-  </Tilt>
+  // </Tilt>
 );
 
 const About = () => {
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Tilt from "react-tilt";
+// import Tilt from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -18,14 +18,14 @@ const ProjectCard = ({
 }) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      <Tilt
+      {/* <Tilt
         options={{
           max: 45,
           scale: 1,
           speed: 450,
         }}
         className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
-      >
+      > */}
         <div className='relative w-full h-[230px]'>
           <img
             src={image}
@@ -62,7 +62,7 @@ const ProjectCard = ({
             </p>
           ))}
         </div>
-      </Tilt>
+      {/* </Tilt> */}
     </motion.div>
   );
 };
