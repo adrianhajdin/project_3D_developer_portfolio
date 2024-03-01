6c17858e903726167ab4de750638de749c4e7859
kartik bhandari <70146959+kartik-bhandari@users.noreply.github.com>
2024-03-02 00:37:05 +0530

    Update Tech.jsx
    This will solve the GLTF problem on android devices by only displaying  limited models when on mobile

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -4,15 +4,35 @@ import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
+
 const Tech = () => {
-  return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
+  const isMobile = window.matchMedia("(max-width: 500px)").matches;
+  const Map = (isMobile ,technologies)=>{
+    if(isMobile){
+      return(
+    technologies.slice(0,4).map((technology)=>(
+    <div className='w-28 h-28' key={technology.name}>
+    <BallCanvas icon={technology.icon}/>
+    <p className="flex justify-center text-white font-bold">{technology.name}</p>
     </div>
+    )))
+    }
+    else{return(
+      technologies.map((technology)=>(
+        <div className='w-28 h-28' key={technology.name}>
+        <BallCanvas icon={technology.icon}/>
+        <p className="flex justify-center text-white font-bold">{technology.name}</p>
+        </div>
+      )))
+    }
+  }
+  return (
+    <>
+    <h1 className='font-bold flex justify-center text-lg mb-5 text-[#915EFF]'>My Tech Stack</h1>
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {Map(isMobile ,technologies)}
+    </div>
+    </>
   );
 };
 
