336750443597d7ee4e3d50208a87539ca5ec5067
AdityaRanjan2005 <145277402+AdityaRanjan2005@users.noreply.github.com>
2024-07-26 17:34:10 +0530

    hehe

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Tilt from "react-tilt";
+import Tilt from "react-parallax-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true
+  }
 })
+
