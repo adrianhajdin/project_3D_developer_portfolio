e914acaefe59cc9b39bd36caf59d2ca67367cc72
AN4SS <117671502+AnassxD@users.noreply.github.com>
2024-09-18 16:17:21 +0100

    EDIT NAVBAR

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             ANASS &nbsp;
-            <span className='sm:block hidden'> | LH</span>
+            <span className='sm:block hidden'>LAHNAOUI</span>
           </p>
         </Link>
 
