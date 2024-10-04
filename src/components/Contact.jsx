8652183b6d81bfef8949875093329a7dd36c391d
Bompalli458 <narasimhulu.b@efundzz.com>
2024-10-04 13:56:47 +0530

    "experience,work flow and contact us reworked"

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,9 +37,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Bompalli Narasimhulu",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "bompallinarasimhulu555@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
--- src/constants/index.js
+++ src/constants/index.js
@@ -45,10 +45,10 @@ const services = [
     title: "Web Developer",
     icon: web,
   },
-  {
-    title: "React Native Developer",
-    icon: mobile,
-  },
+  // {
+  //   title: "React Native Developer",
+  //   icon: mobile,
+  // },
   {
     title: "Backend Developer",
     icon: backend,
@@ -88,10 +88,10 @@ const technologies = [
     name: "Tailwind CSS",
     icon: tailwind,
   },
-  {
-    name: "Node JS",
-    icon: nodejs,
-  },
+  // {
+  //   name: "Node JS",
+  //   icon: nodejs,
+  // },
   {
     name: "MongoDB",
     icon: mongodb,
@@ -116,82 +116,87 @@ const technologies = [
 
 const experiences = [
   {
-    title: "React.js Developer",
-    company_name: "Starbucks",
-    icon: starbucks,
+    title: "Backend Developer",
+    company_name: "EFUNDZZ",
+    icon: "https://www.efundzz.com/EF_Assets/logo.svg",
     iconBg: "#383E56",
-    date: "March 2020 - April 2021",
+    date: "June 2023 - present",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Developing and maintaining RESTful web applications using Spring Boot, PostgreSQL, and Docker.",
+    "Integrating third-party services, APIs, and databases to enhance product capabilities.",
+    "Collaborating with cross-functional teams including designers, product managers, and developers to ensure seamless user experiences.",
+    "Ensuring code quality through regular code reviews, automated testing, and adhering to best practices.",
+    "Working on cloud deployment and CI/CD pipelines to automate testing and deployment using AWS services.",
     ],
   },
-  {
-    title: "React Native Developer",
-    company_name: "Tesla",
-    icon: tesla,
-    iconBg: "#E6DEDD",
-    date: "Jan 2021 - Feb 2022",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "Web Developer",
-    company_name: "Shopify",
-    icon: shopify,
-    iconBg: "#383E56",
-    date: "Jan 2022 - Jan 2023",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "Full stack Developer",
-    company_name: "Meta",
-    icon: meta,
-    iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
+  // {
+  //   title: "React Native Developer",
+  //   company_name: "Tesla",
+  //   icon: tesla,
+  //   iconBg: "#E6DEDD",
+  //   date: "Jan 2021 - Feb 2022",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+  
+  // {
+  //   title: "Web Developer",
+  //   company_name: "Shopify",
+  //   icon: shopify,
+  //   iconBg: "#383E56",
+  //   date: "Jan 2022 - Jan 2023",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+
+  // {
+  //   title: "Full stack Developer",
+  //   company_name: "Meta",
+  //   icon: meta,
+  //   iconBg: "#E6DEDD",
+  //   date: "Jan 2023 - Present",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
 ];
 
 const testimonials = [
   {
     testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
+      "I've never met a developer who truly cares about their clients' success like narasimhulu does.",
+    name: "Eswar Manjunath",
+    designation: "CEO",
+    company: "Efundzz",
+    image: "https://media.licdn.com/dms/image/v2/C5603AQHQM_5LctLMPg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1639997205055?e=1732752000&v=beta&t=I1i5CxvxmjcbMEdXIpugWxwkLRqgT_XZ2sjJl10ZgVw",
+  },
+  // https://media.licdn.com/dms/image/v2/D4E03AQGbfO2DFzOevw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1722206902645?e=1732752000&v=beta&t=nnJijjfgSifs8TveKYbyTJENjxdzeNnxDfA8H4YVacg
+  
+  {
+    testimonial:
+      "I thought it was impossible to make a integrations with third parties as fast as our product, but narasimhulu proved me wrong.",
+    name: "Harinadh Reddy",
+    designation: "Product Owner",
+    company: "Efundzz",
+    image: "https://media.licdn.com/dms/image/v2/D4E03AQGbfO2DFzOevw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1722206902645?e=1732752000&v=beta&t=nnJijjfgSifs8TveKYbyTJENjxdzeNnxDfA8H4YVacg",
   },
   {
     testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
-  },
-  {
-    testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
+      "After narasimhulu optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Bhavani Kommu",
+    designation: "Tech Lead",
+    company: "Efundzz",
     image: "https://randomuser.me/api/portraits/women/6.jpg",
   },
 ];
